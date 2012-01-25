a46047ab419b058957a0e3e9539f8b20ce8a462c
Turpana Molina <develop@turpana.com>
2012-01-25 12:05:42 -0700

    Switched check_cofig to self_config

--- DrupalBackups/drupalbackups.py
+++ DrupalBackups/drupalbackups.py
@@ -11,21 +11,21 @@ class DrupalBackups(object):
   configfile = './drupalservers.json'
 
   def __init__(self):
-    self.check_config()
+    self.self_config()
 
-  def check_config(self):
+  def self_config(self):
     if (not path.isfile(self.configfile)):
-      print 'Config file "drupalservers.json" not found.\n'
+      print 'Warning: config file "drupalservers.json" not found.\n'
       return False
+    self.config()
     return True
 
   def config(self):
-    if (not self.check_config()):
-      return False
     json_data = open(self.configfile)
     data = json.load(json_data)
     self.servers = data['servers']
     json_data.close()
+    return True
 
   def backup_all_now(self):
     print 'Backing up servers'
--- tests/DrupalBackups_tests.py
+++ tests/DrupalBackups_tests.py
@@ -1,6 +1,5 @@
 from nose.tools import *
 from DrupalBackups.drupalbackups import DrupalBackups
-import pprint
 
 def setup():
   print "SETUP!"
@@ -10,13 +9,10 @@ def teardown():
 
 def test_configure():
   backups = DrupalBackups()
-  configfilefound = backups.check_config()
+  configfilefound = backups.self_config()
   assert not configfilefound
   backups.configfile = "./tests/drupalservers.json"
-  configfilefound = backups.check_config()
+  configfilefound = backups.self_config()
   assert configfilefound
-  backups.config()
-  pp = pprint.PrettyPrinter(indent=2)
-  pp.pprint(backups.servers)
   print "I RAN!"
 

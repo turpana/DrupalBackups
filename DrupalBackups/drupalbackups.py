ebde983b19b22152aff5ec04ff69ae73e97cdee3
Turpana Molina <develop@turpana.com>
2012-01-25 11:50:04 -0700

    loading config file

--- DrupalBackups/drupalbackups.py
+++ DrupalBackups/drupalbackups.py
@@ -1,26 +1,31 @@
 # -*- coding:utf-8 -*-
 import fileinput
 from os import path
+import pprint
 try:
   import jsonlib2 as json
 except ImportError:
   import json
 
 class DrupalBackups(object):
-  configfile = 'drupalservers.json'
+  configfile = './drupalservers.json'
 
   def __init__(self):
     self.check_config()
 
   def check_config(self):
-    if (not path.exists(self.configfile)):
+    if (not path.isfile(self.configfile)):
       print 'Config file "drupalservers.json" not found.\n'
       return False
     return True
 
   def config(self):
-    if (self.check_config()):
-      json_data = open(self.configfile)
+    if (not self.check_config()):
+      return False
+    json_data = open(self.configfile)
+    data = json.load(json_data)
+    self.servers = data['servers']
+    json_data.close()
 
-  def backup_all_now():
+  def backup_all_now(self):
     print 'Backing up servers'
--- tests/DrupalBackups_tests.py
+++ tests/DrupalBackups_tests.py
@@ -1,5 +1,6 @@
 from nose.tools import *
 from DrupalBackups.drupalbackups import DrupalBackups
+import pprint
 
 def setup():
   print "SETUP!"
@@ -9,7 +10,13 @@ def teardown():
 
 def test_configure():
   backups = DrupalBackups()
-  configfilefound = backups.config()
+  configfilefound = backups.check_config()
+  assert not configfilefound
+  backups.configfile = "./tests/drupalservers.json"
+  configfilefound = backups.check_config()
   assert configfilefound
+  backups.config()
+  pp = pprint.PrettyPrinter(indent=2)
+  pp.pprint(backups.servers)
   print "I RAN!"
 
